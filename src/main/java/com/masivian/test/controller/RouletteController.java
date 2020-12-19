db6feeb37d218b5c725329ec8bb2643911b2e085
Camilo <capinzonr@unal.edu.co>
2020-12-19 13:02:38 -0500

    Conect to data base

--- src/main/java/com/masivian/test/controller/RouletteController.java
+++ src/main/java/com/masivian/test/controller/RouletteController.java
@@ -1,23 +1,33 @@
 package com.masivian.test.controller;
 
+import com.masivian.test.db.MysqlRoulette;
 import com.masivian.test.dto.Bet;
 import com.masivian.test.dto.Roulette;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class RouletteController {
+
+    private final MysqlRoulette mysqlRoulette;
+
+    public RouletteController(MysqlRoulette mysqlRoulette) {
+        this.mysqlRoulette = mysqlRoulette;
+    }
+
     @PostMapping("roulettes")
-    public int createRoulette(){
-        return 0;
+    public int createRoulette() {
+        return mysqlRoulette.add();
     }
 
     @PutMapping("roulettes/:id")
-    public boolean openRoulette(@PathVariable int id){
+    public boolean openRoulette(@PathVariable int id) {
         return true;
     }
 
     @PostMapping("roulettes/bets")
-    public boolean createBet(@RequestBody Bet bet,@RequestHeader int idUser){
+    public boolean createBet(@RequestBody Bet bet, @RequestHeader int idUser) {
         return false;
     }
 
